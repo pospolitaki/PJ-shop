05512b96416581222684fe621e5c291f96af1385
pospolitaki <pospolitaki@gmail.com>
2018-10-09 20:45:03 +0300

    adds discount

--- static/static_dev/js/product_detail.js
+++ static/static_dev/js/product_detail.js
@@ -15,39 +15,50 @@ $(function() {
     var productPrice = parseFloat(submitBtn.data('price'));
     var quantityInput = $("#quantity-input");
     var totalPrice = document.getElementById("totalPrice");
+    var productDiscount = parseFloat(submitBtn.data('discount'));
+    var discount = 0;
+    var oldPrice = document.getElementById("product-discount");
 
-    
-// Handle the precision up to >= $100
-function changeCost( num ) {
-    var cost = new Number( parseFloat( num ) * productPrice );
-    
-    if ( !isNaN(cost) ) {
-        //var precision = cost.toString().length === 3 ? 3 : 4;
-        //return cost.toPrecision( precision );
-        return cost;
-    } else {
-        return 0;
-    }
+    if (!isNaN(productDiscount) && productDiscount) {
+        discount = productDiscount;
+        }
+
+
+        
+function cost( num ) {
+        var cost = new Number( parseFloat( num ) * productPrice);
+        
+        if ( !isNaN(cost) ) {
+            //var precision = cost.toString().length === 3 ? 3 : 4;
+            //return cost.toPrecision( precision );
+            return cost;
+        } else {
+            return 0;
+        }
 }
-totalPrice.innerHTML = changeCost( quantityInput.val() );    
+
+if (discount) {
+    totalPrice.innerHTML = cost( quantityInput.val() ) - cost( quantityInput.val() )*discount/100;
+    oldPrice.innerHTML = cost( quantityInput.val() );
+    } else {
+        totalPrice.innerHTML = cost( quantityInput.val() );
+    }
+
 quantityInput[0].addEventListener('input', function( event ) {
     var value = event.target.value;
     if ( isNaN( value ) ) {
-    totalPrice.innerHTML = changeCost( 0 );
-    } else {
-        totalPrice.innerHTML = changeCost( value );
-        } 
+    totalPrice.innerHTML = cost( 0 );
+    } else if (discount) {
+        totalPrice.innerHTML = cost( value ) - cost( value )*discount/100;
+        oldPrice.innerHTML = cost( value );
+        } else {
+            totalPrice.innerHTML = cost( value );
+        }
 });
 
 console.log(productPrice);
-
-
+console.log(discount);
 });
-
-
-
-
-
 
 function JewelryAccessoriesFadeIn(){
     $("h2.home").fadeIn(5000);
